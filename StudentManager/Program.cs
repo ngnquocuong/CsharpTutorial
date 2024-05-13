a9e7c8306db5d463324b7667d493d2512d434d03
cuongnq <ngnquocuong@gmail.com>
2024-05-13 15:09:52 +0700

    finish

--- StudentManager/Program.cs
+++ StudentManager/Program.cs
@@ -1,33 +1,64 @@
 ﻿namespace StudentManager
 {
+    using System;
+
     internal class Program
     {
         public static void Main(string[] args)
         {
             var manager = new StudentManager();
             while (true)
-                switch (Validation.GetInt("Choose 1 to 9: ", "Choose an integer from 1 to 8 ", "Try again!!!", 1, 9))
+            {
+                Console.WriteLine("\nCHUONG TRINH QUAN LY SINH VIEN C#");
+                Console.WriteLine("*************************MENU**************************");
+                Console.WriteLine("**  1. Add student.                                  **");
+                Console.WriteLine("**  2. Update student by ID.                         **");
+                Console.WriteLine("**  3. Delete student by ID.                         **");
+                Console.WriteLine("**  4. Search student by name.                       **");
+                Console.WriteLine("**  5. Sort student by grade( GPA).                  **");
+                Console.WriteLine("**  6. Sort student by name.                         **");
+                Console.WriteLine("**  7. Sort student by ID.                           **");
+                Console.WriteLine("**  8. Show student list.                            **");
+                Console.WriteLine("**  0. Exit                                          **");
+                Console.WriteLine("*******************************************************");
+                switch (Validation.GetInt("Choose 0 to 8: ", "Choose an integer from 0 to 8 ", "Try again!!!", 0, 8))
                 {
                     case 1:
                         manager.AddStudent();
 
                         break;
                     case 2:
+                        manager.UpdateStudent();
+
+                        break;
                     case 3:
                         manager.DeleteStudent();
 
                         break;
                     case 4:
-                    case 5:
-                    case 6:
-                    case 7:
-                    case 8:
-                        manager.ShowStudent();
+                        manager.SearchStudentByName();
 
                         break;
-                    case 9:
+                    case 5:
+                        manager.SortStudentByGPA();
+
+                        break;
+                    case 6:
+                        manager.SortStudentByName();
+
+                        break;
+                    case 7:
+                        manager.SortStudentById();
+
+                        break;
+                    case 8:
+                        manager.ShowListStudent();
+
+                        break;
+                    case 0:
                         return;
                 }
+            }
         }
     }
 }
--- StudentManager/StudentManager.cs
+++ StudentManager/StudentManager.cs
@@ -6,6 +6,71 @@ namespace StudentManager
 
     public class StudentManager
     {
+        public StudentManager()
+        {
+            this.list.Add(new Student
+            {
+                Id             = this.GenerateId(),
+                Name           = "John Doe",
+                Sex            = true, // Male
+                Age            = 20,
+                MathScore      = 8.5,
+                PhysicalScore  = 7.0,
+                ChemistryScore = 9.0
+            });
+
+            this.list.Add(new Student
+            {
+                Id             = this.GenerateId(),
+                Name           = "Jane Doe",
+                Sex            = false, // Female
+                Age            = 21,
+                MathScore      = 7.0,
+                PhysicalScore  = 8.0,
+                ChemistryScore = 8.5
+            });
+
+            this.list.Add(new Student
+            {
+                Id             = this.GenerateId(),
+                Name           = "Alex Smith",
+                Sex            = true, // Male
+                Age            = 19,
+                MathScore      = 9.0,
+                PhysicalScore  = 8.5,
+                ChemistryScore = 7.0
+            });
+
+            this.list.Add(new Student
+            {
+                Id             = this.GenerateId(),
+                Name           = "Emily Johnson",
+                Sex            = false, // Female
+                Age            = 22,
+                MathScore      = 6.5,
+                PhysicalScore  = 7.5,
+                ChemistryScore = 8.0
+            });
+
+            this.list.Add(new Student
+            {
+                Id             = this.GenerateId(),
+                Name           = "Michael Brown",
+                Sex            = true, // Male
+                Age            = 20,
+                MathScore      = 8.0,
+                PhysicalScore  = 9.0,
+                ChemistryScore = 8.5,
+            });
+
+            // Calculate MediumScore and AcademicPerformance for each student
+            foreach (var student in this.list)
+            {
+                this.Medium(student);
+                this.AcademicPerformance(student);
+            }
+        }
+
         private readonly List<Student> list = new List<Student>();
 
         private int GenerateId()
@@ -35,6 +100,37 @@ namespace StudentManager
             this.Medium(student);
             this.AcademicPerformance(student);
             this.list.Add(student);
+            Console.WriteLine("Add student successfully!!!");
+        }
+
+        public void UpdateStudent()
+        {
+            this.CheckNullStudent("List is empty to update!!!");
+
+            Console.WriteLine("{0,-5} {1,-20} {2,-5} {3,5} {4,5} {5,5} {6,5} {7,10} {8,10}", "ID", "Name", "Sex", "Age", "Math", "Physical", "Chemistry", "Medium", "Performance");
+
+            this.ShowListStudent();
+
+            var newId           = Validation.GetInt("Student ID to update: ", "Try again!!!", "", 1, list.Count);
+            var studentToUpdate = this.list.FirstOrDefault(student => student.Id == newId);
+
+            if (studentToUpdate == null)
+            {
+                Console.WriteLine("Student with the provided ID does not exist!");
+
+                return;
+            }
+
+            Console.WriteLine("Enter new information:");
+            studentToUpdate.Name           = Validation.GetString("New name: ", "Try again!!!");
+            studentToUpdate.Sex            = Validation.GetBool("New sex (True for Male, False for Female): ", "Try again!!!");
+            studentToUpdate.Age            = Validation.GetInt("New age: ", "Try again!!!", "", 18, 26);
+            studentToUpdate.MathScore      = Validation.GetDouble("New math score: ", "Try again!!!", 0, 10);
+            studentToUpdate.PhysicalScore  = Validation.GetDouble("New physical score: ", "Try again!!!", 0, 10);
+            studentToUpdate.ChemistryScore = Validation.GetDouble("New chemistry score: ", "Try again!!!", 0, 10);
+
+            Console.WriteLine($"Updated student information:\n{studentToUpdate.Id,-5} {studentToUpdate.Name,-20} {studentToUpdate.Sex,-5} {studentToUpdate.Age,5} {studentToUpdate.MathScore,5} {studentToUpdate.PhysicalScore,5} {studentToUpdate.ChemistryScore,5} {studentToUpdate.MediumScore,10} {studentToUpdate.Performance,10}");
+            Console.WriteLine("Update successfully!!!");
         }
 
         private void AcademicPerformance(Student sv)
@@ -51,34 +147,83 @@ namespace StudentManager
             sv.MediumScore = Math.Round(medium, 2, MidpointRounding.AwayFromZero);
         }
 
-        public void ShowStudent()
+        public void ShowListStudent()
         {
-            if (this.list?.Count > 0)
-            {
-                Console.WriteLine("{0,-5} {1,-20} {2,-5} {3,5} {4,5} {5,5} {6,5} {7,10} {8,10}",
-                    "ID", "Name", "Sex", "Age", "Math", "Physical", "Chemistry", "Medium", "Performance");
+            this.CheckNullStudent("List is empty!!!");
 
-                foreach (var student in this.list)
-                {
-                    Console.WriteLine("{0,-5} {1,-20} {2,-5} {3,5} {4,5} {5,5} {6,5} {7,10} {8,10}",
-                        student.Id, student.Name, student.Sex, student.Age, student.MathScore,
-                        student.PhysicalScore, student.ChemistryScore, student.MediumScore, student.Performance);
-                }
-            }
-            else
+            Console.WriteLine("{0,-5} {1,-20} {2,-5} {3,5} {4,5} {5,5} {6,5} {7,10} {8,10}",
+                "ID", "Name", "Sex", "Age", "Math", "Physical", "Chemistry", "Medium", "Performance");
+
+            foreach (var student in this.list)
             {
-                Console.WriteLine("List is empty!!!");
+                Console.WriteLine($"{student.Id,-5} {student.Name,-20} {student.Sex,-5} " + $"{student.Age,5} {student.MathScore,5} {student.PhysicalScore,5} " + $"{student.ChemistryScore,5} {student.MediumScore,10} {student.Performance,10}");
             }
         }
 
         public void DeleteStudent()
         {
+            this.CheckNullStudent("List is empty!!!");
+            this.ShowListStudent();
             var id              = Validation.GetInt("Choose student ID to delete: ", $"Please input a number from 1 to {this.list.Count}!!!", "", 1, this.list.Count);
             var studentToRemove = this.list.FirstOrDefault(student => student.Id == id);
             var message         = studentToRemove != null ? $"Student with ID {id} has been deleted." : $"Student with ID {id} does not exist.";
             this.list.Remove(studentToRemove);
             Console.WriteLine(message);
-            this.ShowStudent();
+            this.ShowListStudent();
+            Console.WriteLine("Delete successfully!!!");
+        }
+
+        public void SearchStudentByName()
+        {
+            this.CheckNullStudent("No student to search!!!");
+
+            var name  = Validation.GetString("Search student name: ", "Try again!!!");
+            var found = false;
+            foreach (var student in this.list)
+            {
+                if (student.Name.IndexOf(name, StringComparison.OrdinalIgnoreCase) < 0) continue;
+                Console.WriteLine($"{student.Id,-5} {student.Name,-20} {student.Sex,-5} " + $"{student.Age,5} {student.MathScore,5} {student.PhysicalScore,5} " + $"{student.ChemistryScore,5} {student.MediumScore,10} {student.Performance,10}");
+                found = true;
+            }
+
+            if (!found)
+            {
+                Console.WriteLine($"No student found with the name '{name}'.");
+            }
+        }
+
+        private void CheckNullStudent(string mess)
+        {
+            if (this.list.Count != 0) return;
+            Console.WriteLine(mess);
+        }
+
+        public void SortStudentByName()
+        {
+            this.CheckNullStudent("No student to sort!!!");
+            this.list.Sort((student, student1) => String.Compare(student.Performance, student1.Performance, StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine("Students sorted by name:");
+            this.ShowListStudent();
+        }
+
+        public void SortStudentByGPA()
+        {
+            this.CheckNullStudent("No student to sort!!!");
+
+            this.list.Sort((student1, student2) => student1.MediumScore.CompareTo(student2.MediumScore));
+
+            Console.WriteLine("Students sorted by GPA:");
+            this.ShowListStudent();
+        }
+
+        public void SortStudentById()
+        {
+            this.CheckNullStudent("No student to sort!!!");
+
+            this.list.Sort((student1, student2) => student1.Id.CompareTo(student2.Id));
+
+            Console.WriteLine("Students sorted by ID:");
+            this.ShowListStudent();
         }
     }
 }
